bea491f3746e6be08f9c5a06e8c9b678461605a2
Ondřej Surý <ondrej@sury.org>
2019-08-07 13:54:47 +0200

    The interbase extension has been moved to PECL.

--- debian/rules.d/ext-interbase.mk
+++ debian/rules.d/ext-interbase.mk
@@ -1,8 +1,7 @@
 ifneq ($(DEB_HOST_ARCH),$(filter $(DEB_HOST_ARCH),hurd-i386 m68k hppa ppc64))
   ext_PACKAGES += interbase
   interbase_DESCRIPTION := Interbase
-  interbase_EXTENSIONS  := interbase pdo_firebird
-  interbase_config      := --with-interbase=shared,/usr
+  interbase_EXTENSIONS  := pdo_firebird
   pdo_firebird_config   := --with-pdo-firebird=shared,/usr
   export interbase_EXTENSIONS
   export interbase_DESCRIPTION
