384cfb4a32966c089119e16e5692cb6fc3f14fd4
Ondřej Surý <ondrej@sury.org>
2019-08-07 13:55:04 +0200

    The configure option for zip extension has changed from --enable-zip to --with-zlib

--- debian/rules.d/ext-zip.mk
+++ debian/rules.d/ext-zip.mk
@@ -2,8 +2,6 @@ ext_PACKAGES    += zip
 zip_DESCRIPTION := Zip
 zip_EXTENSIONS  := zip
 zip_config = \
-	--enable-zip=shared \
-	--with-zlib-dir=/usr \
-	--with-libzip=/usr
+	--with-zip=shared
 export zip_EXTENSIONS
 export zip_DESCRIPTION
