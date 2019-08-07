bdfe16e78522395fb16d6998861b4521cd7825e0
Ondřej Surý <ondrej@sury.org>
2019-08-07 13:58:26 +0200

    The configure options to enable GD extension has changed to --enable-gd and --with-external-gd

--- debian/rules.d/ext-gd.mk
+++ debian/rules.d/ext-gd.mk
@@ -2,12 +2,7 @@ ext_PACKAGES   += gd
 gd_DESCRIPTION := GD
 gd_EXTENSIONS  := gd
 gd_config      := \
-	--with-gd=shared,/usr \
-	--enable-gd-native-ttf \
-	--with-jpeg-dir=/usr \
-	--with-xpm-dir=/usr/X11R6 \
-	--with-png-dir=/usr \
-	--with-freetype-dir=/usr \
-	--with-webp-dir=/usr
+	--enable-gd=shared,/usr \
+	--with-external-gd
 export gd_EXTENSIONS
 export gd_DESCRIPTION
