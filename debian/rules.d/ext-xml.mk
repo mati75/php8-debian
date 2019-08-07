e32815079a675460c134de375650d2d7ed883fa2
Ondřej Surý <ondrej@sury.org>
2019-08-07 12:42:27 +0200

    Configure option --with-libxml-dir is now named --with-libxml

--- debian/rules.d/ext-xml.mk
+++ debian/rules.d/ext-xml.mk
@@ -3,23 +3,23 @@ xml_DESCRIPTION := DOM, SimpleXML, WDDX, XML, and XSL
 xml_EXTENSIONS  := dom simplexml wddx xml xmlreader xmlwriter xsl
 dom_config = \
 	--enable-dom=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 simplexml_config = \
 	--enable-simplexml=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 wddx_config = \
 	--enable-wddx=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 xml_config = \
 	--enable-xml=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 xml_PRIORITY := 15
 xmlreader_config = \
 	--enable-xmlreader=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 xmlwriter_config = \
 	--enable-xmlwriter=shared \
-	--with-libxml-dir=/usr
+	--with-libxml=/usr
 xsl_config      := --with-xsl=shared,/usr
 export xml_PRIORITY
 export xml_EXTENSIONS
