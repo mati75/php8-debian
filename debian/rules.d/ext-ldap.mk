bdbd48d31387c3876950023ff4e9c31471f6405d
Ondřej Surý <ondrej@sury.org>
2019-08-07 15:55:35 +0200

    Update the configure options according to UPGRADING file (mostly pkg-config related changes)

--- debian/rules.d/ext-ldap.mk
+++ debian/rules.d/ext-ldap.mk
@@ -2,6 +2,6 @@ ext_PACKAGES     += ldap
 ldap_DESCRIPTION := LDAP
 ldap_EXTENSIONS  := ldap
 ldap_config      := --with-ldap=shared,/usr \
-	            --with-ldap-sasl=/usr
+	            --with-ldap-sasl
 export ldap_EXTENSIONS
 export ldap_DESCRIPTION
--- debian/rules.d/ext-xml.mk
+++ debian/rules.d/ext-xml.mk
@@ -1,23 +1,13 @@
 ext_PACKAGES    += xml
 xml_DESCRIPTION := DOM, SimpleXML, XML, and XSL
 xml_EXTENSIONS  := dom simplexml xml xmlreader xmlwriter xsl
-dom_config = \
-	--enable-dom=shared \
-	--with-libxml=/usr
-simplexml_config = \
-	--enable-simplexml=shared \
-	--with-libxml=/usr
-xml_config = \
-	--enable-xml=shared \
-	--with-libxml=/usr
+dom_config = --enable-dom=shared
+simplexml_config = --enable-simplexml=shared
+xml_config = --enable-xml=shared
 xml_PRIORITY := 15
-xmlreader_config = \
-	--enable-xmlreader=shared \
-	--with-libxml=/usr
-xmlwriter_config = \
-	--enable-xmlwriter=shared \
-	--with-libxml=/usr
-xsl_config      := --with-xsl=shared,/usr
+xmlreader_config = --enable-xmlreader=shared
+xmlwriter_config = --enable-xmlwriter=shared
+xsl_config      := --with-xsl=shared
 export xml_PRIORITY
 export xml_EXTENSIONS
 export xml_DESCRIPTION
