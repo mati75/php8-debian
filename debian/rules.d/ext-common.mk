2eb4f219c12672f689cee92dd142b3c3ba17a669
Ondřej Surý <ondrej@sury.org>
2019-08-28 10:23:15 +0200

    Enable FFI experimental extension

--- debian/rules.d/ext-common.mk
+++ debian/rules.d/ext-common.mk
@@ -1,10 +1,11 @@
 ext_PACKAGES     += common
 common_DESCRIPTION := documentation, examples and common
-common_EXTENSIONS  := calendar ctype exif fileinfo ftp gettext iconv pdo phar posix shmop sockets sysvmsg sysvsem sysvshm tokenizer
+common_EXTENSIONS  := calendar ctype exif fileinfo ffi ftp gettext iconv pdo phar posix shmop sockets sysvmsg sysvsem sysvshm tokenizer
 calendar_config = --enable-calendar=shared
 ctype_config = --enable-ctype=shared
 exif_config = --enable-exif=shared
 fileinfo_config = --enable-fileinfo=shared
+ffi_config = --with-ffi=shared
 ftp_config = --enable-ftp=shared --with-openssl-dir=/usr
 gettext_config = --with-gettext=shared,/usr
 iconv_config = --with-iconv=shared
