862a7c90316d06bd40242f7673e3febf51a00368
Ondřej Surý <ondrej@sury.org>
2020-10-07 10:59:09 +0200

    Specify the socket via ./configure option rather than patch

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -2,8 +2,8 @@ ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
 mysql_EXTENSIONS  := mysqlnd mysqli pdo_mysql
 mysqlnd_config    := --enable-mysqlnd=shared --enable-mysqlnd-compression-support --with-zlib-dir=/usr
-mysqli_config     := --with-mysqli=shared,mysqlnd
-pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
+mysqli_config     := --with-mysqli=shared,mysqlnd --with-mysql-sock=/var/run/mysqld/mysqld.sock
+pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd --with-mysql-sock=/var/run/mysqld/mysqld.sock
 mysqlnd_PRIORITY  := 10
 export mysqlnd_PRIORITY
 export mysql_EXTENSIONS
